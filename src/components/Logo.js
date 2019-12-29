0e2f090843256e7072213defa1491ddd37c5c767
albondad <36680782+albondad@users.noreply.github.com>
2019-12-28 19:32:16 -0600

    updated logo, removed about page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logoImage from '../logo.jpg';
 
 let logo = () => {
   return(
     <div id='logoContainer'>
-      <span id='logoText'>bl<span id='logoIcon'><i className="fas fa-clock"></i></span>k</span>
+      {/*<span id='logoText'>bl<span id='logoIcon'><i className="fas fa-clock"></i></span>k</span>*/}
+      <img id='logoImage' src={logoImage} />
     </div>
   )
 }
